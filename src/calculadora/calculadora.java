b4c15ddc910d34c6f81a5a9f4976443502db1ac7
ivanruquet <63323177+ivanruquet@users.noreply.github.com>
2020-04-20 22:59:46 -0300

    tercer commit/ cambio if-else por switch
    by ivan ruquet

--- src/calculadora/calculadora.java
+++ src/calculadora/calculadora.java
@@ -11,22 +11,31 @@ public class calculadora {
 		Integer resultado;
 		Scanner teclado = new Scanner(System.in);
 		do{
-			System.out.println("Ingrese su operación /n 1 para sumar /n 2 para restar /n 3 para multiplicar /n 4 para dividir");
+			System.out.println("Ingrese su operaciÃ³n /n 1 para restar /n 2 para multiplicar /n 3 para dividir /n 4 para sumar");
 			opcion = Integer.parseInt(teclado.next());
-		}while(opcion < 1 && opcion > 4);
-		System.out.println("Ingrese el primer numero");
-		a = Integer.parseInt(teclado.next());
-		System.out.println("Ingrese el segundo numero");
-		b = Integer.parseInt(teclado.next());
-		if(opcion.equals(1)){
-			resultado = a - b;
-			}else if(opcion.equals(2)){
-				resultado = a * b;
-				}else if(opcion.equals(3)){
-					resultado = a / b;
-					}else{
-						resultado = a + b;
-						} 
+		}
+            while (opcion < 1 && opcion > 4);
+            System.out.println("Ingrese el primer numero");       
+            a = teclado.nextInt();
+           
+            System.out.println("Ingrese el segundo numero");  
+            b = teclado.nextInt();
+           
+            switch (opcion) {
+         
+            case 1:
+            	resultado = a - b; 
+            break;
+            case 2:
+            	resultado = a * b;
+           break;
+           case 3:
+        	   resultado = a / b;   
+            break;
+           case 4:
+        	   resultado = a + b;
+           break;
+            }
  
             System.out.println("El resultado es " + resultado.toString());
             }
